d7736f2fd636378c3cdd393023cc71a2566126ae
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-05-30 11:20:54 -0300

    tarefa: removendo botão de merge caso o dono do merge seja diferente do usuário logado

--- src/js/hideButton.js
+++ src/js/hideButton.js
@@ -1,5 +1,20 @@
-var isTammy = document.querySelector('.author_link .author').textContent.indexOf('Tammy');
+((doc) => {
 
-if(!~isTammy) {
-  document.querySelector('.mr-widget-body .js-merge-button').style.display = 'none';
-};
+  const mergeOwner = () => {
+    const elementAuthor = doc.querySelector('.author_link')
+    return elementAuthor.getAttribute('href').substring(3)
+  }
+  
+  const loggedUser = () => {
+    return doc.querySelector('.username').textContent.trim()
+  }
+
+  const hideMergeButton = () => {
+    doc.querySelector('.mr-widget-body').style.display = 'none'
+  }
+  
+  if( mergeOwner() !== loggedUser() ) {
+     hideMergeButton()
+  }
+
+})(document)
